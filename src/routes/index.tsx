cdc34b9b16645911844a62b2321edd67fbcb157e
IE-HE <eda@infoearth.co.jp>
2025-10-26 11:19:49 +0900

    fix: from export to import

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,16 +4,16 @@ import { createSignal } from "solid-js";
 // components
 import { Button } from "~/components/button";
 import { FileDrop } from "~/components/form";
-export { ProgressCircle } from "~/components/progress";
+import { ProgressCircle } from "~/components/progress";
 
 export default function Home() {
-  const [loading, setLoading] = createSignal(false);
+  const [loading, setLoading] = createSignal<boolean>(false);
   
   return (
     <div>
       <FileDrop />
       <div class="my-4" />
-      <Button label="Click Me" onClick={() => alert("Button Clicked!")} />
+      <Button label="Click Me" onClick={() => {setLoading(prev => {return !prev});}} />
       <div class="my-4" />
       <ProgressCircle loading={loading()} />
     </div>
