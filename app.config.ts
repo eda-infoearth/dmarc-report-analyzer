f786893bb1687dc7bdb4f25484f2adb28f8505af
Hiroki Eda <eda@infoearth.co.jp>
2025-10-31 13:16:09 +0900

    rollback: app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -7,40 +7,13 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   ssr: true, // ← SPA構成
-  // server: {
-  //   baseURL: "/dmarc-report-analyzer/",
-  //   static: true,
-  //   prerender: {
-  //     failOnError: true,
-  //     routes: ["/"],
-  //     crawlLinks: true,
-  //   },
-  // },
+  server: {
+    baseURL: "/dmarc-report-analyzer/",
+    static: true,
+    prerender: {
+      failOnError: true,
+      routes: ["/"],
+      crawlLinks: true,
+    },
+  },
 });
-
-
-// async function loadEnv() {
-//   const mod = await import("./src/loadEnv");
-//   return mod.env;
-// }
-
-// const loadedEnv = await loadEnv();
-
-// const isDev = loadedEnv.IS_DEVELOPMENT;
-// console.log("isDev:", isDev);
-
-// export default defineConfig({
-//   vite: {
-//     plugins: [tailwindcss()],
-//   },
-//   ssr: isDev ? true : false, // ← SPA構成
-//   server: isDev ? undefined : {
-//     baseURL: "/dmarc-report-analyzer/",
-//     static: true,
-//     prerender: {
-//       failOnError: true,
-//       routes: ["/"],
-//       crawlLinks: true,
-//     },
-//   },
-// });
